5d769fdbae5606c571f75516b6b72c0de7cd0873
Arian Kordi <ariankordi@ariankordi.net>
2026-05-28 19:37:58 -0400

    Add ESLint and tsconfig.

---
+++ eslint.config.mjs
@@ -0,0 +1,180 @@
+import eslint from '@eslint/js';
+import eslintCommentPlugin from '@eslint-community/eslint-plugin-eslint-comments/configs';
+import stylisticPlugin from '@stylistic/eslint-plugin';
+import globals from 'globals';
+import jsdoc from 'eslint-plugin-jsdoc';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import cdkLabsPlugin from '@cdklabs/eslint-plugin';
+
+const stylisticConfig = stylisticPlugin.configs.customize({
+	indent: 'tab',
+	quotes: 'single',
+	semi: true,
+	commaDangle: 'never',
+	braceStyle: '1tbs'
+});
+
+const ecmaVersion = 2022;
+
+export default [
+	// https://eslint.org/docs/rules/
+	eslint.configs.recommended,
+	{
+		languageOptions: {
+			ecmaVersion,
+			sourceType: 'module',
+			globals: {
+				...globals.browser
+			}
+		},
+		rules: {
+			'require-atomic-updates': 'off', // This rule is widely controversial and causes false positives
+			'no-console': 'off',
+			'prefer-const': ['error', {
+				destructuring: 'all' // Only error if all destructured variables can be const
+			}],
+			'no-var': 'error',
+			// TODO: 'no-unused-vars': ['error', { argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^ignore' }],
+			'one-var': ['error', 'never'],
+			'curly': ['error', 'all'], // Always require curly braces
+
+			'class-methods-use-this': 'error',
+			'no-return-assign': 'error',
+			'template-curly-spacing': 'error',
+			// See: https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/no-useless-undefined.md#conflict-with-eslint-array-callback-return-and-getter-return-rules
+			'getter-return': ['error', { allowImplicit: true }]
+			// The following will trigger a bunch of errors:
+			// 'no-param-reassign': 'error', // This is used but ig alternative is larger code
+			// 'no-use-before-define': 'error', // We don't have imports right now
+			// 'func-names': 'error', // While ()=>{} is preferred, sometimes this is needed
+			// 'no-underscore-dangle': 'warn', // I prefer underscore to # right now as it's not always supported
+			// 'no-unused-expressions': 'warn', // This is the (condition) && function() substitute for one-line if
+			// 'new-cap': 'error', // Three.js node materials definitely violate this
+			// 'no-plusplus': 'warn', // This is very standard convention in JS, C/C++
+		}
+	},
+
+	// https://eslint-community.github.io/eslint-plugin-eslint-comments/rules/
+	eslintCommentPlugin.recommended,
+	{
+		rules: {
+			'@eslint-community/eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+			'@eslint-community/eslint-comments/require-description': 'error'
+		}
+	},
+
+	// https://typescript-eslint.io/rules/
+	// (tseslint.configs.recommended: unused)
+
+	// https://eslint.style/rules
+	stylisticConfig,
+	{
+		rules: {
+			'@stylistic/no-extra-semi': 'error',
+			'@stylistic/yield-star-spacing': ['error', 'after'],
+			'@stylistic/operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
+			'@stylistic/curly-newline': ['error', {
+				multiline: true,
+				consistent: true
+			}],
+			'@stylistic/object-curly-newline': ['error', {
+				multiline: true,
+				consistent: true
+			}],
+			'@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: false }],
+			'@stylistic/indent': ['error', 'tab', {
+				SwitchCase: 1,
+				ignoredNodes: ['Program > ExpressionStatement > CallExpression > :last-child > *']
+			}],
+			'@stylistic/max-len': ['warn', {
+				code: 100,
+				comments: 120,
+				ignoreUrls: true, // Ignore long URLs
+				ignoreStrings: true, // Ignore long strings
+				ignoreTemplateLiterals: true, // Ignore long template literals
+				ignoreRegExpLiterals: true, // Ignore regex
+				ignoreTrailingComments: false, // Enforce trailing comment length
+				ignoreComments: false // Enforce all comment lines
+			}]
+		}
+	},
+
+	// https://www.npmjs.com/package/eslint-plugin-unicorn
+	eslintPluginUnicorn.configs.unopinionated,
+	{
+		rules: {
+			// Not a fan of the numeric separators, since I don't think those are a thing in C/C++.
+			'unicorn/numeric-separators-style': 'off',
+			// The below enforces that hex is always uppercase.
+			'unicorn/number-literal-case': 'off',
+			// 'unicorn/no-static-only-class': 'off',
+			'unicorn/prefer-string-replace-all': 'off', // ES2021 only
+			'unicorn/no-array-sort': 'off', // ES2023 only
+			'unicorn/prefer-code-point': 'off', // Nullability does not match
+			// This is, in fact, only called in CLIs.
+			'unicorn/no-process-exit': 'off'
+		}
+	},
+
+	// https://github.com/cdklabs/eslint-rules
+	// Pretty much only actively using this for "no-this-in-static".
+	{
+		plugins: {
+			// ... other plugins
+			'@cdklabs': cdkLabsPlugin
+		},
+		rules: {
+			'@cdklabs/no-core-construct': ['error'],
+			'@cdklabs/invalid-cfn-imports': ['error'],
+			'@cdklabs/no-literal-partition': ['error'],
+			'@cdklabs/no-invalid-path': ['error'],
+			// '@cdklabs/no-throw-default-error': ['error'],
+			'@cdklabs/promiseall-no-unbounded-parallelism': ['error'],
+			'@cdklabs/no-this-in-static': ['error'],
+			'@cdklabs/no-unconditional-token-allocation': ['error']
+		}
+	},
+
+	// https://www.npmjs.com/package/eslint-plugin-jsdoc
+	jsdoc.configs['flat/recommended'],
+	{
+		settings: {
+			jsdoc: {
+				mode: 'typescript', // For templates and type "&" syntax.
+				// Use `Object` for single and `Object<>` for multiple. That works in TS and Closure.
+				preferredTypes: { 'object': 'Object', 'object.<>': 'Object<>', 'Object.<>': 'Object<>', 'object<>': 'Object<>' }
+			}
+		},
+		rules: {
+			// Lots of structs don't need detailed descriptions.
+			'jsdoc/require-property-description': 'off',
+
+			// Rules not enabled in recommended:
+			'jsdoc/sort-tags': 'warn',
+			'jsdoc/require-throws': 'warn',
+			'jsdoc/require-template': 'warn',
+			'jsdoc/check-template-names': 'warn',
+			// Require hyphen before param descriptions but not before return description.
+			'jsdoc/require-hyphen-before-param-description': ['warn', 'always', { tags: { returns: 'never' } }],
+			'jsdoc/require-asterisk-prefix': 'warn',
+			'jsdoc/check-syntax': 'warn',
+			'jsdoc/check-line-alignment': 'warn',
+			'jsdoc/check-indentation': 'warn',
+			'jsdoc/convert-to-jsdoc-comments': 'warn',
+			'jsdoc/multiline-blocks': ['warn', {
+				noMultilineBlocks: true,
+				minimumLengthForMultiline: 80,
+				multilineTags: []
+			}]
+
+		}
+	},
+
+	{
+		ignores: [
+			// Defaults
+			'**/dist/', // Common build output directory
+			'**/*.min.js' // Minified JavaScript files
+		]
+	}
+];
