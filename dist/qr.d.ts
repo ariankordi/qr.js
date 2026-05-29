e17d3cdb5d0c2fd55f801bcad196e8ea116f7e93
Arian Kordi <ariankordi@ariankordi.net>
2026-05-28 21:00:00 -0400

    only include qr.js in tsconfig.json and add qr.d.ts

---
+++ dist/qr.d.ts
@@ -0,0 +1,221 @@
+export default QRCode;
+export type InputData = string | ArrayLike<number>;
+declare namespace QRCode {
+    /** @typedef {'numeric'|'alphanumeric'|'octet'} ModeParam */
+    /** @typedef {'L'|'M'|'Q'|'H'} EccLevelParam */
+    /**
+     * @typedef {Object} QRCodeOptions
+     * @property {number} [version] - Version in [1,40]; defaults to auto-select.
+     * @property {ModeParam} [mode] - One of 'numeric', 'alphanumeric', 'octet'; defaults to auto-select.
+     * @property {EccLevelParam} [ecclevel] - One of 'L', 'M', 'Q', 'H'; defaults to 'L'.
+     * @property {number} [mask] - Mask in [0,7]; defaults to auto-select.
+     * @property {number} [modulesize] - Size of each module in pixels; defaults to 5px.
+     * @property {number} [margin] - Margin in modules; defaults to 4.
+     * @property {string} [unit] - Unit for non-px sizes (e.g., 'mm', 'cm'); defaults to 'px'.
+     * @property {number} [ratio] - Ratio for non-px sizes; defaults to 1.
+     */
+    function generate(data: InputData, options?: {
+        /**
+         * - Version in [1,40]; defaults to auto-select.
+         */
+        version?: number;
+        /**
+         * - One of 'numeric', 'alphanumeric', 'octet'; defaults to auto-select.
+         */
+        mode?: "numeric" | "alphanumeric" | "octet";
+        /**
+         * - One of 'L', 'M', 'Q', 'H'; defaults to 'L'.
+         */
+        ecclevel?: "L" | "M" | "Q" | "H";
+        /**
+         * - Mask in [0,7]; defaults to auto-select.
+         */
+        mask?: number;
+        /**
+         * - Size of each module in pixels; defaults to 5px.
+         */
+        modulesize?: number;
+        /**
+         * - Margin in modules; defaults to 4.
+         */
+        margin?: number;
+        /**
+         * - Unit for non-px sizes (e.g., 'mm', 'cm'); defaults to 'px'.
+         */
+        unit?: string;
+        /**
+         * - Ratio for non-px sizes; defaults to 1.
+         */
+        ratio?: number;
+    }): number[][];
+    function generateHTML(data: InputData, options?: {
+        /**
+         * - Version in [1,40]; defaults to auto-select.
+         */
+        version?: number;
+        /**
+         * - One of 'numeric', 'alphanumeric', 'octet'; defaults to auto-select.
+         */
+        mode?: "numeric" | "alphanumeric" | "octet";
+        /**
+         * - One of 'L', 'M', 'Q', 'H'; defaults to 'L'.
+         */
+        ecclevel?: "L" | "M" | "Q" | "H";
+        /**
+         * - Mask in [0,7]; defaults to auto-select.
+         */
+        mask?: number;
+        /**
+         * - Size of each module in pixels; defaults to 5px.
+         */
+        modulesize?: number;
+        /**
+         * - Margin in modules; defaults to 4.
+         */
+        margin?: number;
+        /**
+         * - Unit for non-px sizes (e.g., 'mm', 'cm'); defaults to 'px'.
+         */
+        unit?: string;
+        /**
+         * - Ratio for non-px sizes; defaults to 1.
+         */
+        ratio?: number;
+    }): HTMLDivElement;
+    function generateSVG(data: InputData, options?: {
+        /**
+         * - Version in [1,40]; defaults to auto-select.
+         */
+        version?: number;
+        /**
+         * - One of 'numeric', 'alphanumeric', 'octet'; defaults to auto-select.
+         */
+        mode?: "numeric" | "alphanumeric" | "octet";
+        /**
+         * - One of 'L', 'M', 'Q', 'H'; defaults to 'L'.
+         */
+        ecclevel?: "L" | "M" | "Q" | "H";
+        /**
+         * - Mask in [0,7]; defaults to auto-select.
+         */
+        mask?: number;
+        /**
+         * - Size of each module in pixels; defaults to 5px.
+         */
+        modulesize?: number;
+        /**
+         * - Margin in modules; defaults to 4.
+         */
+        margin?: number;
+        /**
+         * - Unit for non-px sizes (e.g., 'mm', 'cm'); defaults to 'px'.
+         */
+        unit?: string;
+        /**
+         * - Ratio for non-px sizes; defaults to 1.
+         */
+        ratio?: number;
+    }): SVGSVGElement;
+    function generateCanvas(data: InputData, options?: {
+        /**
+         * - Version in [1,40]; defaults to auto-select.
+         */
+        version?: number;
+        /**
+         * - One of 'numeric', 'alphanumeric', 'octet'; defaults to auto-select.
+         */
+        mode?: "numeric" | "alphanumeric" | "octet";
+        /**
+         * - One of 'L', 'M', 'Q', 'H'; defaults to 'L'.
+         */
+        ecclevel?: "L" | "M" | "Q" | "H";
+        /**
+         * - Mask in [0,7]; defaults to auto-select.
+         */
+        mask?: number;
+        /**
+         * - Size of each module in pixels; defaults to 5px.
+         */
+        modulesize?: number;
+        /**
+         * - Margin in modules; defaults to 4.
+         */
+        margin?: number;
+        /**
+         * - Unit for non-px sizes (e.g., 'mm', 'cm'); defaults to 'px'.
+         */
+        unit?: string;
+        /**
+         * - Ratio for non-px sizes; defaults to 1.
+         */
+        ratio?: number;
+    }): HTMLCanvasElement;
+    function generateImage(data: InputData, options: {
+        /**
+         * - Version in [1,40]; defaults to auto-select.
+         */
+        version?: number;
+        /**
+         * - One of 'numeric', 'alphanumeric', 'octet'; defaults to auto-select.
+         */
+        mode?: "numeric" | "alphanumeric" | "octet";
+        /**
+         * - One of 'L', 'M', 'Q', 'H'; defaults to 'L'.
+         */
+        ecclevel?: "L" | "M" | "Q" | "H";
+        /**
+         * - Mask in [0,7]; defaults to auto-select.
+         */
+        mask?: number;
+        /**
+         * - Size of each module in pixels; defaults to 5px.
+         */
+        modulesize?: number;
+        /**
+         * - Margin in modules; defaults to 4.
+         */
+        margin?: number;
+        /**
+         * - Unit for non-px sizes (e.g., 'mm', 'cm'); defaults to 'px'.
+         */
+        unit?: string;
+        /**
+         * - Ratio for non-px sizes; defaults to 1.
+         */
+        ratio?: number;
+    } | undefined, type: Parameters<typeof HTMLCanvasElement.prototype.toDataURL>[0]): string;
+    function generatePNG(data: InputData, options?: {
+        /**
+         * - Version in [1,40]; defaults to auto-select.
+         */
+        version?: number;
+        /**
+         * - One of 'numeric', 'alphanumeric', 'octet'; defaults to auto-select.
+         */
+        mode?: "numeric" | "alphanumeric" | "octet";
+        /**
+         * - One of 'L', 'M', 'Q', 'H'; defaults to 'L'.
+         */
+        ecclevel?: "L" | "M" | "Q" | "H";
+        /**
+         * - Mask in [0,7]; defaults to auto-select.
+         */
+        mask?: number;
+        /**
+         * - Size of each module in pixels; defaults to 5px.
+         */
+        modulesize?: number;
+        /**
+         * - Margin in modules; defaults to 4.
+         */
+        margin?: number;
+        /**
+         * - Unit for non-px sizes (e.g., 'mm', 'cm'); defaults to 'px'.
+         */
+        unit?: string;
+        /**
+         * - Ratio for non-px sizes; defaults to 1.
+         */
+        ratio?: number;
+    }): string;
+}
